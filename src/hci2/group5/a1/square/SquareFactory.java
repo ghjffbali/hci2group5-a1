cf50b8fcb16ada8544a22d9304f6f3bcb366aab0
DerekMartens <33992280+DerekMartens@users.noreply.github.com>
2019-02-04 16:59:44 -0600

    Update SquareFactory.java

--- src/hci2/group5/a1/square/SquareFactory.java
+++ src/hci2/group5/a1/square/SquareFactory.java
@@ -17,25 +17,35 @@ public class SquareFactory {
 
 	public static HitableSquare getRandomTargetSquare(MainView mainView, Square startSquare, SquareAmplitude amplitude, SquareSize size) {
 
-		int length = size.length;
-		int x, y;
-		
-		int halfLength = size.length / 2;
+        int length = size.length;
+        int x, y;
 
-		int maxX = mainView.getWidth() - halfLength;
-		int maxY = mainView.getHeight() - halfLength;
-		int x0 = startSquare.getCenterX();
-		int y0 = startSquare.getCenterY();
-		int a = amplitude.value;
-		
-		x = RandomInt.generateWithin(halfLength, maxX);
+        int halfLength = size.length / 2;
 
-		y = (int) (y0 + Math.abs(x0 - x) - a);
-		while(y > maxY || y < halfLength) {
-			x = RandomInt.generateWithin(halfLength, maxX);
-			y = (int) (y0 + Math.abs(x0 - x) - a);
-		}
-		
-		return new HitableSquare(x, y, length);
-	}
+        int maxX = mainView.getWidth() - halfLength;
+        int maxY = mainView.getHeight() - halfLength;
+        int x0 = startSquare.getCenterX();
+        int y0 = startSquare.getCenterY();
+        int a = amplitude.value;
+        int a2 = aa;
+
+
+        x = RandomInt.generateWithin(halfLength, maxX);
+        int deltaX = x0-x;
+        y = (int) (y0 - Math.sqrt(a2 - (deltaXdeltaX)));
+
+        while(y > maxY || y < halfLength) {
+            x = RandomInt.generateWithin(halfLength, maxX);
+            deltaX = x0-x;
+            y = (int) (y0 - Math.sqrt(a2 - (deltaX*deltaX)));
+        }
+
+        int deltaY = y0-y;
+
+
+        System.out.println("amplitude is " +a +", y is " +y +", y0 is " +y0 +", x is " +x + ", and x0 is " +x0);
+        System.out.println("amplitude^2 is " +a2 +", delta y is " +deltaY +", delta x is " +deltaX);
+        System.out.println("maxX is " +maxX);
+        return new HitableSquare(x, y, length);
+    }
 }
